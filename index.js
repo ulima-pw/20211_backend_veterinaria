9d62552a4ce4ecde64bba225541c5006ec4693f0
Hernan <hernan@jsatch.com>
2022-06-30 13:56:29 -0500

    Implementando endpoint con filtros

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from "express"
+import { Mascota } from "./dao/index.js"
 
 const PORT = 5000
 const app = express()
 
 // Endpoint: Obtener lista de mascotas
-// GET /mascotas 
-app.get("/mascotas", (req, resp) => {
-
+// GET /mascotas?edad=5 
+app.get("/mascotas", async (req, resp) => {
+    const edadMascota = req.query.edad
+    let mascotas;
+    if (edadMascota == undefined){
+        mascotas = await Mascota.findAll()
+    }else {
+        mascotas = await Mascota.findAll({
+            where : {
+                edad : edadMascota
+            }
+        })
+    }
+    resp.send(JSON.stringify(mascotas))
 })
 
 app.listen(PORT, () => {
