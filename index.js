c5c4b66f65376ce934c5350ccf9702ff43878ed2
Hernan <hernan@jsatch.com>
2022-07-02 19:47:53 -0500

    Implementacion de API REST para entidad mascota

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express"
+import bodyParser from "body-parser"
 import { Mascota } from "./dao/index.js"
 
 const PORT = 5000
 const app = express()
+app.use(bodyParser.json()) // configuracion para recibir json por post (cuerpo)
+
+//============================================================================================
 
 // Endpoint: Obtener lista de mascotas
 // GET /mascotas?edad=5 
@@ -21,11 +25,67 @@ app.get("/mascotas", async (req, resp) => {
     resp.send(JSON.stringify(mascotas))
 })
 
+// Endpoint: Obtener una mascota segun el id
+// GET "/mascota/<ID_MASCOTA>"
+app.get("/mascota/:id", async (req, resp)=> {
+    const idMascota = req.params.id
+
+    const mascota = await Mascota.findByPk(idMascota)
+
+    if (mascota != null) {
+        resp.send("OK")
+    }else {
+        resp.status(400).send("ERROR: No existe id de mascota")
+    }
+})
+
 // Endpoint: Agregar una nueva mascota
 // POST /mascotas
-app.post("/mascotas", (req, resp) => {
+app.post("/mascotas", async (req, resp) => {
+    const mascota = req.body
+    
+    await Mascota.create({
+        nombre : mascota.nombre,
+        edad : mascota.edad,
+        birthday : mascota.birthday
+    })
+
     resp.send("OK")
 })
+
+// Endpoint: Modificar una mascota ya existente
+// PUT /mascotas
+app.put("/mascotas", async (req, resp) => {
+    const mascota = req.body
+
+    await Mascota.update(mascota, {
+        where : {
+            id : mascota.id
+        }
+    })
+
+    resp.send("OK")
+})
+
+// Endpoint: Eliminar una mascota ya existente
+// DELETE /mascotas
+app.delete("/mascotas/:id", async (req, resp) => {
+    const idMascota = req.params.id
+
+    const filasDestruidas = await Mascota.destroy({
+        where : {
+            id : idMascota
+        }
+    })
+
+    if (filasDestruidas > 0) {
+        resp.send("OK")
+    }else {
+        resp.status(400).send("ERROR: No existe id de mascota")
+    }
+})
+
+//============================================================================================
 
 app.listen(PORT, () => {
     console.log(`Servidor inicial en puerto ${PORT}`)
